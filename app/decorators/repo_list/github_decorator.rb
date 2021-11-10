118112e74aaaeed84ab967c8daf78f3261148a3d
fabriond <gfbf@ic.ufal.br>
2021-11-10 18:49:23 -0300

    feat(RepoList::GithubDecorator): added decorator for github's response

---
+++ app/decorators/repo_list/github_decorator.rb
@@ -0,0 +1,40 @@
+class RepoList::GithubDecorator
+  # According to https://docs.github.com/v3/search/ only the first 1000 search results are available
+  MAX_AVAILABLE_SEARCH_RESULTS = 1000
+  DEFAULT_API_PAGE = 1
+  DEFAULT_API_PER_PAGE = 30
+  
+  attr_reader :current_page_items, :total_count, :page, :per_page
+
+  def initialize(api_response_body:, page:, per_page:)
+    @current_page_items = decorated_items(api_response_body)
+    @total_count = decorated_total_count(api_response_body)
+    @page = page.blank? ? DEFAULT_API_PAGE : page
+    @per_page = per_page.blank? ? DEFAULT_API_PER_PAGE : per_page
+  end
+
+  def pagination_info
+    {
+      current_page_items: current_page_items,
+      total_count: total_count,
+      page: page,
+      per_page: per_page
+    }
+  end
+
+  private
+
+  def decorated_items(api_response_body)
+    api_response_body["items"].map do |repo|
+      {
+        name: repo["full_name"], 
+        description: repo["description"], 
+        url: repo["html_url"]
+      }
+    end
+  end
+
+  def decorated_total_count(api_response_body)
+    [api_response_body["total_count"].to_i, MAX_AVAILABLE_SEARCH_RESULTS].min
+  end
+end
